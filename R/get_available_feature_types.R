d07337b2d8e2b407de7c39fd72538e5d1437dac3
tkoncz <tamas.koncz@emarsys.com>
2019-11-30 22:35:04 +0100

    :truck: move `get_available_feature_types()` into separate .R file

---
+++ R/get_available_feature_types.R
@@ -0,0 +1,18 @@
+#' @title helper function code to record available feature types
+#'
+#' @return a list of available features names and their types (as returned by the API)
+#'
+#' @examples
+#' get_available_feature_types()
+#'
+#' @export
+get_available_feature_types <- function() {
+    list(
+        LABEL_DETECTION         = "labelAnnotations",
+        TEXT_DETECTION          = "textAnnotations",
+        DOCUMENT_TEXT_DETECTION = "textAnnotations",
+        FACE_DETECTION          = "faceAnnotations",
+        LOGO_DETECTION          = "logoAnnotations",
+        LANDMARK_DETECTION      = "landmarkAnnotations"
+    )
+}
--- R/get_google_vision_response.R
+++ R/get_google_vision_response.R
@@ -255,25 +255,6 @@ extract_error <- function(responses, imagePaths) {
     )
 }
 
-#' @title helper function code to record available feature types
-#'
-#' @return a list of available features names and their types (as returned by the API)
-#'
-#' @examples
-#' get_available_feature_types()
-#'
-#' @export
-get_available_feature_types <- function() {
-    list(
-        LABEL_DETECTION         = "labelAnnotations",
-        TEXT_DETECTION          = "textAnnotations",
-        DOCUMENT_TEXT_DETECTION = "textAnnotations",
-        FACE_DETECTION          = "faceAnnotations",
-        LOGO_DETECTION          = "logoAnnotations",
-        LANDMARK_DETECTION      = "landmarkAnnotations"
-    )
-}
-
 #' @title helper function code to provide an extractor function for different feature types
 #' @description a utility to provide functions to extract features from the API response
 #'
